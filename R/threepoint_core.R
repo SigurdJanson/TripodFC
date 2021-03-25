0bd609e4ac57f65d5449cb416211ba6ddf673d0f
SigurdJanson <emailjs@web.de>
2021-03-25 22:40:24 +0100

    App code

---
+++ R/threepoint_core.R
@@ -0,0 +1,320 @@
+#'
+#' Basic app functions for "3-point Estimator" App
+#' 
+#'
+library(distr)
+library(mc2d)
+
+
+#' @details The approach by Davis avoids assumptions regarding 
+Pert.Params <- function(min=-1, mode=0, max=1, shape = 4, method = c("classic", "vose", "davis")) {
+  ## check input
+  if (!exists("min")) stop("'min' is missing")
+  if (!exists("mode")) stop("'mode' is missing")
+  if (!exists("max")) stop("'max' is missing")
+  if (!exists("shape")) stop("'shape' is missing")
+  if (!is.numeric(min)) stop("'min' must be a numeric value")
+  if (!is.numeric(mode)) stop("'mode' must be a numeric value")
+  if (!is.numeric(max)) stop("'max' must be a numeric value")
+  if (!is.numeric(shape)) stop("'shape' must be a numeric value")
+  
+  if (!exists("method")) stop("'method' is missing")
+  method <- match.arg(method)
+  
+  if (method == "classic") {
+    mu <- (min + shape * mode + max) / (shape + 2)
+    sdev <- (max - min) / (shape + 2)
+    alpha <- ((mu - min) / (max - min)) * ( ((mu - min) * (max - mu) / (sdev^ 2 )) - 1 )
+    beta <- alpha * (max - mu) / (mu - min)
+  }
+  
+  if (method == "vose") {
+    mu <- (min + shape * mode + max) / (shape + 2)
+    alpha <- ifelse(mu == mode,
+                    1 + shape / 2, # avoid div/0
+                    ((mu - min) * (2 * mode - min - max)) / ((mode - mu) * (max - min))) #ok
+    beta <- alpha * (max - mu) / (mu - min)
+  }
+  if (method == "davis") {
+    alpha <- (2 * (max + 4*mode - 5*min) / (3*(max-min))) *
+      (1 + 4 * ( (mode-min)*(max-mode)/(max-min)^2 ))
+    beta <- alpha * (5*max-4*mode-min) / (max+4*mode-5*min)
+  }
+  if(is.null(alpha)) stop("Method not available")
+    
+  out <- list(alpha = alpha, beta = beta,
+              min = min, mode = mode, max = max,
+              method = method)
+  class(out) <- "betaPERT"
+  
+  return(out)
+}
+
+
+#' betaPERT
+#' Parametrize a generalized Beta distribution
+#' @param min Pessimistic estimate (Minimum value)
+#' @param mode Most likely estimate (Mode)
+#' @param max Optimistic estimate (Maximum value)
+#' @param shape Scale parameter
+#' @param method "classic" or "vose"; see details below
+#' @author Brecht Devleesschauwer <\email{brechtdv@@gmail.com}> 
+betaPERT <- function(min=-1, mode=0, max=1, shape = 4, method = c("classic", "vose")) {
+    ## check input
+    if (!exists("min")) stop("'min' is missing")
+    if (!exists("mode")) stop("'mode' is missing")
+    if (!exists("max")) stop("'max' is missing")
+    if (!exists("shape")) stop("'shape' is missing")
+    if (!is.numeric(min)) stop("'min' must be a numeric value")
+    if (!is.numeric(mode)) stop("'mode' must be a numeric value")
+    if (!is.numeric(max)) stop("'max' must be a numeric value")
+    if (!is.numeric(shape)) stop("'shape' must be a numeric value")
+  
+    if (!exists("method")) stop("'method' is missing")
+    method <- match.arg(method)
+    
+    if (method == "classic") {
+      mu <- (min + shape * mode + max) / (shape + 2)
+      sdev <- (max - min) / (shape + 2)
+      alpha <- ((mu - min) / (max - min)) * ( ((mu - min) * (max - mu) / (sdev^ 2 )) - 1 )
+      beta <- alpha * (max - mu) / (mu - min)
+    }
+    
+    if (method == "vose") {
+      mu <- (min + shape * mode + max) / (shape + 2)
+      alpha <- ifelse(mu == mode, 
+                      1 + shape / 2, # avoid div/0
+                      ((mu - min) * (2 * mode - min - max)) / ((mode - mu) * (max - min)))
+      beta <- alpha * (max - mu) / (mu - min)
+    }
+    
+    out <- list(alpha = alpha, beta = beta,
+                min = min, mode = mode, max = max,
+                method = method)
+    class(out) <- "betaPERT"
+    
+    return(out)
+  }
+
+#' #' dpert
+#' #' @description Density function for the PERT (aka Beta PERT) distribution with minimum 
+#' #' equals to min, mode equals to mode and maximum equals to max.
+#' #' @usage dpert(x, min=-1, mode=0, max=1, shape=4, log=FALSE)
+#' #' @param x Vector of quantiles.
+#' #' @param min Vector of minima.
+#' #' @param mode Vector of modes.
+#' #' @param max Vector of maxima.
+#' #' @param shape	Vector of scaling parameters. Default value: 4.
+#' #' @param log Logical; if TRUE, probabilities p are given as log(p).
+#' #' @details Taken from the 'mc2d' package
+#' #' @author Regis Pouillot
+#' #' @author Matthew Wiener
+#' #' @references R. Pouillot, M.-L. Delignette-Muller (2010), Evaluating variability and uncertainty in 
+#' #' microbial quantitative risk assessment using two R packages. International Journal of Food Microbiology. 142(3):330-40 
+#' dpert <- function (x, min = -1, mode = 0, max = 1, shape = 4, log = FALSE) 
+#' {
+#'   if (length(x) == 0) 
+#'     return(numeric(0))
+#'   
+#'   min <- as.vector(min)
+#'   mode <- as.vector(mode)
+#'   max <- as.vector(max)
+#'   shape <- as.vector(shape)
+#' 
+#'   a1 <- 1 + shape * (mode - min)/(max - min)
+#'   a2 <- 1 + shape * (max - mode)/(max - min)
+#'   
+#'   oldw <- options(warn = -1)
+#'   d <- (x - min)^(a1 - 1) * (max - x)^(a2 - 1) / 
+#'     beta(a = a1, b = a2) / 
+#'     (max - min)^(a1 + a2 - 1)
+#'   options(warn = oldw$warn)
+#'   
+#'   d[x < min | x > max] <- 0
+#'   d[mode < min | max < mode] <- NaN
+#'   d[shape <= 0] <- NaN
+#'   
+#'   if (log) d <- log(d)
+#'   if (any(is.na(d))) 
+#'     warning("NaN in dpert")
+#'   
+#'   return(d)
+#' }
+#' 
+#' 
+#' #' qpert
+#' #' @description Quantile function for the PERT (aka Beta PERT) distribution with minimum 
+#' #' equals to min, mode equals to mode and maximum equals to max.
+#' #' @usage qpert(p, min=-1, mode=0, max=1, shape=4, lower.tail=TRUE, log.p=FALSE)
+#' #' @param p Vector of probabilities.
+#' #' @param min Vector of minima.
+#' #' @param mode Vector of modes.
+#' #' @param max Vector of maxima.
+#' #' @param shape	Vector of scaling parameters. Default value: 4.
+#' #' @param log.p Logical; if TRUE, probabilities p are given as log(p).
+#' #' @param lower.tail Logical; if TRUE (default), probabilities are P[X <= x], otherwise, P[X > x].
+#' #' @details Taken from the 'mc2d' package
+#' #' @author Regis Pouillot
+#' #' @author Matthew Wiener
+#' #' @references R. Pouillot, M.-L. Delignette-Muller (2010), Evaluating variability and uncertainty in 
+#' #' microbial quantitative risk assessment using two R packages. International Journal of Food Microbiology. 142(3):330-40 
+#' qpert <- function (p, min = -1, mode = 0, max = 1, shape = 4, lower.tail = TRUE, 
+#'           log.p = FALSE) 
+#' {
+#'   if (length(p) == 0)
+#'     return(numeric(0))
+#'   min <- as.vector(min)
+#'   mode <- as.vector(mode)
+#'   max <- as.vector(max)
+#'   shape <- as.vector(shape)
+#' 
+#'   lout <- max(length(p), length(min), length(mode), length(max), length(shape))
+#'   min <- rep(min, length.out = lout)
+#'   mode <- rep(mode, length.out = lout)
+#'   max <- rep(max, length.out = lout)
+#'   shape <- rep(shape, length.out = lout)
+#' 
+#'   if (log.p) 
+#'     p <- exp(p)
+#'   if (!lower.tail) 
+#'     p <- 1 - p
+#' 
+#'   a1 <- 1 + shape * (mode - min)/(max - min)
+#'   a2 <- 1 + shape * (max - mode)/(max - min)
+#'   
+#'   oldw <- options(warn = -1)
+#'   q <- qbeta(p, shape1 = a1, shape2 = a2)
+#'   options(warn = oldw$warn)
+#'   
+#'   q <- q * (max - min) + min
+#'   minmodemax <- (abs(min - max) < (.Machine$double.eps^0.5))
+#'   q <- ifelse(minmodemax, min, q)
+#'   q[p < 0 | p > 1] <- NaN
+#'   q[mode < min | max < mode] <- NaN
+#'   q[shape <= 0] <- NaN
+#'   
+#'   if (any(is.na(q))) 
+#'     warning("NaN in qpert")
+#'   return(q)
+#' }
+
+
+
+#' weighted.gmean
+#' @param x an object containing the values whose weighted mean is to be computed.
+#' @param w	a numerical vector of weights the same length as x giving the weights 
+#' to use for elements of x.
+#' @param na.rm	a logical value indicating whether NA values in x should be 
+#' stripped before the computation proceeds.
+#' @details Returns NaN when all weights are 0.
+#' @value A length-one numeric vector.
+weighted.gmean <- function(x, w, scl = 1, na.rm = FALSE) {
+  # PRECONDITIONS
+  if(any(x < 0, na.rm = TRUE)) return(NaN)
+  if(missing(w)) w <- rep(1, length(x))
+  if(all(w == 0, na.rm = TRUE)) return(NaN)
+  if(length(x) != length(w)) stop("Weights w must have the same length as x")
+  if(na.rm) {
+    w <- w[!is.na(x)]
+    if(length(w) == 0) return(NaN)
+    x <- x[!is.na(x)]
+  }
+  
+  # CODE
+  num <- sum(w * log(x)) # numerator
+  den <- sum(w) # denominator
+  return(scl * exp(num/den))
+}
+
+
+
+#' weighted.prod
+#' @param x an object containing the values whose weighted mean is to be computed.
+#' @param w	a numerical vector of weights the same length as x giving the weights 
+#' to use for elements of x.
+#' @param na.rm	a logical value indicating whether NA values in x should be 
+#' stripped before the computation proceeds.
+#' @note This function is not finished/correct.
+#' @details Returns NaN when all weights are 0.
+#' @value A length-one numeric vector.
+weighted.prod <- function(x, w, scl = 1, na.rm = FALSE) {
+  # PRECONDITIONS
+  if(missing(w)) w <- rep(1, length(x))
+  if(all(w == 0, na.rm = TRUE)) return(NaN)
+  if(length(x) != length(w)) stop("Weights w must have the same length as x")
+
+  # CODE
+  return(scl * prod(x^w, na.rm))
+}
+
+
+#' PoolOpinions
+#' Pools opinions according to the rationale laid out by Dietrich & List (2016).
+#' @param Values A matrix with Pert distributions as rows
+#' @param Weights Default: unweighted pooling
+#' @param Method Arithmetic, geometric or multiplicative pooling. Default is 'Arithmetic'.
+#' @details Instead of more complex pooling methods, this approach sticks to
+#' the simple solutions that perform well, according to Clemen & Winkler (1999).
+#' Weights must be non-negative (Clemen & Winkler, 1999). They will normalized be 
+#' \code{PoolOpinions} so that they sum up to 1.
+#' @references Dietrich, F. & List, C. (2016). Probabilistic Opinion Pooling. 
+#' In A. Hajek & C. Hitchcock (eds.), Oxford Handbook of Philosophy and Probability. 
+#' Oxford: Oxford University Press
+#' Clemen, R.T. & Winkler R.L. (1999). Combining Probability Distributions 
+#' From Experts in Risk Analysis. Risk Analysis 19, 187–203. doi:10.1023/A:1006917509560
+PoolOpinions <- function( Values, Weights = rep(1/nrow(Values), nrow(Values)), 
+                          Method = c("Arithmetic", "Geometric", "Multiplicative") ) {
+  if(!is.matrix(Values)) { #try to coerce
+    Values <- as.matrix(Values)
+  }
+  if(nrow(Values) < 2) {
+    warning("Parameter 'Values' has only 1 row. A single vector cannot be pooled with itself.")
+    return(Values)
+  }
+  if(any(Weights < 0)) stop("Weights must be > 1")
+  if(sum(Weights != 1)) { #normalize weights if required
+    Weights <- Weights / sum(Weights)
+  }
+  
+  Method <- match.arg( Method )
+  switch(Method,
+         Arithmetic     = apply(Values, 2, weighted.mean,  Weights),
+         Geometric      = apply(Values, 2, weighted.gmean, Weights), 
+         Multiplicative = apply(Values, 2, weighted.prod,  Weights),
+         apply(Values, 2, weighted.mean,  Weights)
+         )
+}
+
+
+#' Area50P
+#' Computes the average of the integral function based on empirical data.
+#' @param X A numeric vector
+#' @param Values 'histogram' data, i.e. observations based on some probability distribution
+#' (\code{Values = f(X)} with \code{f} being an arbitrary density function).
+#' @return The value of \code{X} at which the integral of the probability distribution 
+#' exceeds 50% of it's area.
+Area50P <- function(X, Values) {
+  if(length(X) != length(Values)) stop("X and Values must have the same length")
+  if(length(X) == 1) {
+    warning("A distribution of length 1 cannot be divided in half")
+    return(1)
+  }
+  Percentages <- Values / sum(Values)
+  Accumulated <- cumsum(Percentages)
+  
+  # if there is an exact match for 0.5 take this exact index ...
+  Index50 <- which(Accumulated == 0.5)
+  # ... otherwise interpolate
+  if(identical(Index50, integer(0))) {
+    Index50 <- min(which(Accumulated > 0.5))  # get index where value goes over .5
+    
+    # Get an interpolated value: BaseX(<50) + (DeltaOfX(<50, >50) * Weight)
+    Threshold50 <- X[Index50-1] + (X[Index50] - X[Index50-1]) * 
+      ((0.5 - Accumulated[Index50-1]) / (Accumulated[Index50] - Accumulated[Index50-1]))
+  } else {
+    Threshold50 <- X[Index50] # convert index to x-value
+  }
+  
+  return(Threshold50)
+}
+
--- app.R
+++ app.R
@@ -1,49 +1,215 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#' Shiny App to do PERT 3-point estimates
+#' 
+#' 
+library("shiny")
+library("shinyMatrix")
+if(!exists("PoolOpinions", mode="function")) 
+    source("threepoint_core.R")
 
-library(shiny)
+#' Constant to define into how many pieces the distribution will be split. 
+#' The more pieces, the more precision.
+Precision <- 100000
 
-# Define UI for application that draws a histogram
+# Define UI for app that draws a histogram ----
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    # App title ----
+    titlePanel("Three-Point Estimation"),
+    
+    tabsetPanel(
+        id = "tabs",
+        # Single Panel  ----
+        tabPanel("Single", "Three-point estimation for a single person",
+                 sidebarLayout(
+                     sidebarPanel(
+                         
+                         sliderInput(inputId = "Optimistic0",
+                                     label = "Optimistic:",
+                                     min = 1,
+                                     max = 200,
+                                     value = 15),
+                         sliderInput(inputId = "Typical0",
+                                     label = "Typical:",
+                                     min = 1,
+                                     max = 200,
+                                     value = 25,
+                                     step = 1),
+                         sliderInput(inputId = "Pessimistic0",
+                                     label = "Pessimistic:",
+                                     min = 1,
+                                     max = 200,
+                                     value = 50)
+                     ), #sidebarPanel
+                     
+                     # Main panel for displaying outputs ----
+                     mainPanel(
+                         plotOutput(outputId = "SinglePlot"),
+                         textOutput('safeError')
+                     )
+                 ),
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        # Team Panel  ----
+        tabPanel("Team", "Combined team estimates",
+                 sidebarLayout(
+                     sidebarPanel(
+                         matrixInput("TeamMatrix",
+                                     rows = list(names = TRUE, editableNames = TRUE, extend = TRUE),
+                                     cols = list(names = TRUE, editableNames = FALSE),
+                                     value = matrix(c(11,12,8,9,13,13,10,10,17,16,13,15), 4, 3,
+                                                    dimnames = list(c("Peter", "Jane", "Paul", "Susan"),
+                                                                    c("Optimal", "Typical", "Pessimistic"))),
+                                     #value = matrix(1:6, 2, 3,
+                                     #                dimnames = list(c("MA1", "MA2"),
+                                     #                                c("Opt", "Typ", "Pess"))),
+                                     class = "numeric"
+                         )
+                     ),
+                     # Main panel for displaying outputs ----
+                     mainPanel(
+                         plotOutput(outputId = "MultiPlot"),
+                         #verbatimTextOutput("CombiStats"),
+                         #textOutput(outputId = "CombiAverage"),
+                         #textOutput(outputId = "CombiStdDev"),
+                         #textOutput(outputId = "CombiVar"),
+                         #textOutput(outputId = "CombiSkewness"),
+                         #textOutput(outputId = "CombiKurtosis"),
+                         plotOutput(outputId = "CombiPlot")
+                     )
+                 )
         )
-    )
+    )#tabsetPanel
 )
 
-# Define server logic required to draw a histogram
+
+
+# Server ----
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    # Single PERT distribution ----
+    # with optimistic, typical and pessimistic value
+    output$SinglePlot <- renderPlot({
+        shiny::validate(
+            need(input$Typical0 > input$Optimistic0,     
+                 "'Typical' must be larger than 'Optimistic'"),
+            need(input$Pessimistic0 > input$Typical0, 
+                 "'Pesimistic' must be larger than 'Typical'")
+        )
+        
+        X <- seq(input$Optimistic0, input$Pessimistic0, length.out=Precision)
+        # plot PERT distribution
+        plot(X, dpert(X, min=input$Optimistic0, mode=input$Typical0, max=input$Pessimistic0),
+             type = "l", 
+             main = "Estimated Distribution", xlab = "Estimate", ylab = "Subjective Probability")
+        
+        # Add the three point estimates (opt., typical, pess.)
+        points(c(input$Optimistic0, input$Typical0, input$Pessimistic0), c(0,0,0), 
+               pch = 17, col = "red", cex = 3)
+        
+        UnitX <- range(X) / 100
+        #unitY #TODO
+        
+        # Explicit 3-point estimate using the integral
+        Average <- qpert(0.5, min=input$Optimistic0, mode=input$Typical0, max=input$Pessimistic0)
+        abline(v = Average, col = "blue")
+        text(Average-0.75, y = 0, labels = round(Average, 2), cex = 1, col = "blue")
+        
+        # Weighted 3-point estimate
+        Average <- (input$Optimistic0 + 4L*input$Typical0 + input$Pessimistic0) / 6
+        abline(v = Average)
+        text(Average+0.5, y = 0.01, labels = round(Average, 2), cex = 1, col = "black")
     })
+    
+    
+    # Team Data Set ----
+    # Generate the data set required for the Team tab 
+    MultiPertData <- reactive({
+        Values <- na.omit( input$TeamMatrix )
+        Estimators <- nrow(Values)
+        shiny::validate(
+            need(Estimators > 0, "I need at least one triple of estimates to show something")
+        )
+        # Values on x axis
+        X <- seq(min(Values), max(Values), length.out = Precision)
+        
+        
+        # Compute all PERT functions
+        for(Line in 1:nrow(Values)) {
+            dfunc <- function(x) dpert(x, min  = Values[Line, "Optimal"], 
+                                       mode = Values[Line, "Typical"], 
+                                       max  = Values[Line, "Pessimistic"])
+            D[Line] <- AbscontDistribution(d = dfunc, withgaps = FALSE)#, withStand = TRUE)
+        }
+        rownames(Data) <- rownames(Values)
+        
+        # 
+        # PooledData <- PoolOpinions(Data, Weights = Weight)
+        MixedDistr <- UnivarMixingDistribution(Norm(3, .25), Norm(1, .5))
+        #plot(MixedDistr)
+        
+        # Add 50% threshold of pooled distribution
+        Threshold50 <- Area50P(X, PooledData)
+        
+        
+        list(
+            Estimates = Values,
+            PertData = Data,
+            PooledPertData = PooledData,
+            Threshold50 = Threshold50,
+            XValues = X,         # x axis
+            Weights = Weight
+        )
+    })
+    
+    
+    # Multiple Pert distributions ----
+    # Plot a chart with several Pert distributions in it 
+    output$MultiPlot <- renderPlot({
+        PertData  <- MultiPertData()$PertData
+        Estimates <- MultiPertData()$Estimates
+        X <- MultiPertData()$Xvalues
+        Weights <- MultiPertData()$Weights
+        
+        # Determine appropriate size of y-axis
+        MaxY <- max(PertData)
+        # Determine values on x-axis
+        X <- seq( min(Estimates), max(Estimates), length.out = length(PertData[1,]) )
+        
+        
+        # plot first PERT distribution
+        plot(X, PertData[1,],
+             type = "l", ylim = c(0, MaxY),
+             main = "Individual Distributions", xlab = "Estimate", ylab = "Subjective Probability")
+        
+        # plot additional distributions
+        if(nrow(PertData) > 1) {
+            for(Line in 2:nrow(PertData))
+                lines(X, PertData[Line,], col="black", lty=Line)
+        }
+        
+        legend("topright", legend=rownames( Estimates ), lty=1:nrow(Estimates), cex=0.8)
+    })
+    
+    
+    
+    # Pooled distribution ----
+    # Plot a chart after distributions have been pooled together
+    output$CombiPlot <- renderPlot({
+        Pooled <- MultiPertData()$PooledPertData
+        Estimates <- MultiPertData()$Estimates
+        
+        # Get values for x axis
+        X <- seq( min(Estimates), max(Estimates), length.out = length(Pooled) )
+        
+        plot(X, Pooled,
+             type = "l", 
+             main = "Combined Distribution", xlab = "Estimate", ylab = "Subjective Probability")
+        
+        abline(v = MultiPertData()$Threshold50, col = "blue")
+        text(MultiPertData()$Threshold50, y = 0, labels = round(MultiPertData()$Threshold50, 2), cex = 1, col = "blue")
+    })
+    
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+
+#
+# Run the App ----
+shinyApp(ui, server)
